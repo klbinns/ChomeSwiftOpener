ae94847249c475cc4d0238282fc8b7ba7310fc28
Kyle <kyle@gmail.com>
2014-09-23 22:16:40 -0400

    Fixed background.js bug

--- js/background.js
+++ js/background.js
@@ -3,12 +3,13 @@ chrome.browserAction.onClicked.addListener(function (activeTab) {
     chrome.storage.sync.get({
         tabs: []
     }, function (items) {
+
         var tabs = items.tabs;
 
         if (tabs.length > 0) {
             var i = 0;
             for (i; i < tabs.length; i++) {
-                chrome.tabs.create({url: tabs[i]});
+                chrome.tabs.create({url: tabs[i].url});
             }
 
         } else {
