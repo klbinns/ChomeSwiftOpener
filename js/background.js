1cc9200fe27fc0438f30363cd99816f463b57302
klbinns <kyle.l.binns@gmail.com>
2014-10-17 23:44:44 -0400

    Improvements to browserAction

--- js/background.js
+++ js/background.js
@@ -7,18 +7,31 @@ chrome.browserAction.onClicked.addListener(function (activeTab) {
         var tabs = items.tabs;
 
         if (tabs.length > 0) {
+
             var i = 0;
             for (i; i < tabs.length; i++) {
                 chrome.tabs.create({url: tabs[i].url});
             }
 
-        } else {
-            var toOptions =confirm("No URLs configured for Swift Open!\n\nSet some URLs now?");
-            if(toOptions){
-                chrome.tabs.create({url: "html/options.html"});
-            }
         }
 
     });
+
+});
+
+
+chrome.storage.sync.get({
+        tabs: []
+}, function (items) {
+
+    var tabs = items.tabs;
+
+    if (tabs.length > 0) {
+        chrome.browserAction.setPopup({popup: ""});
+    } else {
+        chrome.browserAction.setPopup({popup: "../html/popup.html"});
+    }
+
+    chrome.browserAction.setBadgeText({text: tabs.length.toString()});
 
 });
